99cea72f41cfe6b85c360a757853a6746e943073
Hadhemi33 <benmansourhadhemi01@gmail.com>
2024-02-01 14:45:33 +0100

    first component

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Title = () => {
+  return (
+    
+        <Text>Hello</Text>
+      
+  );
+};
+export default Title;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default Home;
